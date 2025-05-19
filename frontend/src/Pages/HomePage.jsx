10e3ff1f91efe53a83270349560188a01a48b3b4
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-05-19 13:46:20 +0200

    npm u

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div>
-      <main className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+      <main className='flex flex-col items-center justify-center bg-gray-100'>
         <h1 className='text-4xl font-bold mb-4'>Welcome to the Home Page</h1>
         <p className='text-lg'>This is a simple home page layout.</p>
       </main>
