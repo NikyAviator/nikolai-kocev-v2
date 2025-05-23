8c0ecc2414ea4f203143c258292272c3a790762a
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-05-23 21:43:19 +0200

    AboutmeSec updated!

--- frontend/src/Components/sections/AboutmeSection.jsx
+++ frontend/src/Components/sections/AboutmeSection.jsx
@@ -29,7 +29,7 @@ export default function AboutmeSection() {
     <div className='relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32'>
       <img
         alt=''
-        src='https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply'
+        src='/images/airplane.jpg'
         className='absolute inset-0 -z-10 size-full object-cover object-right md:object-center'
       />
       <div className='hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl'>
@@ -53,12 +53,10 @@ export default function AboutmeSection() {
       <div className='mx-auto max-w-7xl px-6 lg:px-8'>
         <div className='mx-auto max-w-2xl lg:mx-0'>
           <h2 className='text-5xl font-semibold tracking-tight text-white sm:text-7xl'>
-            Support center
+            Nikolai Kocev
           </h2>
           <p className='mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8'>
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat.
+            Software Developer and Pilot from Lund Sweden.
           </p>
         </div>
         <div className='mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8'>
