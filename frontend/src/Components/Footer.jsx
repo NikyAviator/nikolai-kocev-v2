1bd04a7241c42efaf79900c38a39feef61dcb2dc
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-05-23 15:50:40 +0200

    Footer text-indigo-600 also

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className='mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8'>
         <div className='flex items-center gap-x-4 md:order-2'>
           {/* Text to the left of icons */}
-          <p className='text-sm text-gray-400'>
+          <p className='text-sm text-indigo-600'>
             Follow me on social media and github:
           </p>
 
@@ -21,7 +21,7 @@ const Footer = () => {
         </div>
 
         {/* Copyright Text */}
-        <p className='mt-8 text-center text-sm text-gray-400 md:order-1 md:mt-0'>
+        <p className='mt-8 text-center text-sm text-indigo-600 md:order-1 md:mt-0'>
           &copy; 2025 Made by: NikyAviator. All rights reserved.
         </p>
       </div>
