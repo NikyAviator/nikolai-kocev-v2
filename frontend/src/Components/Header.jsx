e3d7c73322d8ebf66ed0cbe7521f5933b9387bc5
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-05-23 15:40:03 +0200

    text-indigo-600 Header.jsx

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className='bg-white'>
+    <header className='bg-gray-900'>
       <nav
         aria-label='Global'
         className='flex items-center justify-between p-6 lg:px-8'
@@ -27,7 +27,7 @@ export default function Header() {
           <button
             type='button'
             onClick={() => setMobileMenuOpen(true)}
-            className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700'
+            className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-indigo-600'
           >
             <span className='sr-only'>Open main menu</span>
             <Bars3Icon aria-hidden='true' className='size-6' />
@@ -38,7 +38,7 @@ export default function Header() {
             <Link
               key={item.name}
               to={item.to}
-              className='text-sm/6 font-semibold text-gray-900'
+              className='text-sm/6 font-semibold text-indigo-600'
             >
               {item.name}
             </Link>
@@ -62,7 +62,7 @@ export default function Header() {
             <button
               type='button'
               onClick={() => setMobileMenuOpen(false)}
-              className='-m-2.5 rounded-md p-2.5 text-gray-700'
+              className='-m-2.5 rounded-md p-2.5 text-indigo-600'
             >
               <span className='sr-only'>Close menu</span>
               <XMarkIcon aria-hidden='true' className='size-6' />
@@ -75,7 +75,7 @@ export default function Header() {
                   <Link
                     key={item.name}
                     to={item.to}
-                    className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50'
+                    className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-indigo-600 hover:bg-gray-50'
                   >
                     {item.name}
                   </Link>
