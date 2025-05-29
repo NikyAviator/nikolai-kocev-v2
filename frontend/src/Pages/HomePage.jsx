294ca1e42aa6cfce60e73beba73c437c92c83a50
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-05-29 19:34:07 +0200

    mv pic*.jpg and addwed AboutMeSec.jsx

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import AboutmeSection from "../Components/sections/AboutmeSection";
+import AboutMeSec from "../Components/sections/AboutMeSec";
 
 const HomePage = () => {
   return (
     <div>
       <AboutmeSection />
+      <AboutMeSec />
     </div>
   );
 };
