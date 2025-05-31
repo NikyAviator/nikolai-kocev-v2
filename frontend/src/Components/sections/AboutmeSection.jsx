d871e9d8403f22d63d378b3a4785f49b6daf08e7
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-05-31 19:13:28 +0200

    npm u and updated text first section

--- frontend/src/Components/sections/AboutmeSection.jsx
+++ frontend/src/Components/sections/AboutmeSection.jsx
@@ -2,24 +2,24 @@ import {
   LifebuoyIcon,
   NewspaperIcon,
   PhoneIcon,
-} from "@heroicons/react/20/solid";
+} from '@heroicons/react/20/solid';
 
-import Card from "../ui/Card";
+import Card from '../ui/Card';
 
 const cards = [
   {
-    name: "Resume",
-    description: "See my full professional CV and education history",
+    name: 'Resume',
+    description: 'See my full professional CV and education history',
     icon: LifebuoyIcon,
   },
   {
-    name: "Projects",
-    description: "Explore my DevOps & Fullstack portfolio",
+    name: 'Projects',
+    description: 'Explore my DevOps & Fullstack portfolio',
     icon: NewspaperIcon,
   },
   {
-    name: "Contact",
-    description: "Let’s connect and build something amazing",
+    name: 'Contact',
+    description: 'Let’s connect and build something amazing',
     icon: PhoneIcon,
   },
 ];
@@ -36,7 +36,7 @@ export default function AboutmeSection() {
         <div
           style={{
             clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
           }}
           className="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20"
         />
@@ -45,7 +45,7 @@ export default function AboutmeSection() {
         <div
           style={{
             clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
           }}
           className="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20"
         />
@@ -55,10 +55,17 @@ export default function AboutmeSection() {
           <h2 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">
             Nikolai Kocev
           </h2>
-          <p className="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
-            Software developer and Pilot – I’m a developer and flight instructor
-            based in Lund, Sweden, passionate about building scalable,
-            real-world software with purpose.
+          <p className="mt-8 space-y-6 rounded-xl bg-black/40 p-6 text-white backdrop-blur-md">
+            Developer, pilot, and lifelong learner based in Lund, Sweden.
+          </p>
+          <p className="mt-8 space-y-6 rounded-xl bg-black/40 p-6 text-white backdrop-blur-md">
+            I first got hands-on experience with “the cloud” as a flight
+            instructor, helping people take off — literally. But I wanted to
+            explore a different kind of altitude, so I taught myself programming
+            and dove into DevOps, cloud platforms, and automation. I’m
+            passionate about Linux, open source, and making systems run smarter
+            and safer. I love tinkering and building tools that make life a
+            little easier.
           </p>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8">
