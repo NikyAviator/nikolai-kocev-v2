a5c7d067ac8c9e16eb56a86c0d0d284c353270a7
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-06-11 21:26:40 +0200

    basic app.js server working

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,23 @@
+// Makes use of .env file for configuration
 require('dotenv').config();
-console.log(process.env.PORT);
+// Starts the basic express server
+const express = require('express');
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Init of CORS and the use of the cors middleware
+const cors = require('cors');
+app.use(cors());
+
+// Basic route to check if the server is running
+app.get('/', (req, res) => {
+  res.send('Server is running, amazing!');
+});
+
+// Server listening on the port defined in the .env file or defaulting to 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
