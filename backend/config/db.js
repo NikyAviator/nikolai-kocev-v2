2a7baabd3cee0fc338ae818d135d73dcbcc0ec18
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-07-30 21:01:32 +0200

    mongodb db.js created

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+export async function main(){
+    try {
+       await mongoose.connect(process.env.MONGODB_URI )
+        console.log('Connected to MongoDB successfully');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);     
+    }
+}
