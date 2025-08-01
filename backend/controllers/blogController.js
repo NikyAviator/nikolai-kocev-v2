3faa205d67a461f53c0d4b24c72ab6774dc6c4f3
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-08-01 12:56:11 +0200

    clean slate for understanding purpose

--- backend/controllers/blogController.js
+++
@@ -1,8 +0,0 @@
-import Blog from '../models/Blog.js';
-
-export const getAllBlogs = async (req, res) => {};
-
-export const updateBlog = async (req, res) => {};
-
-export const deleteBlog = async (req, res) => {};
-
--- backend/controllers/userController.js
+++
@@ -1,5 +0,0 @@
-import User from '../models/User.js';
-
-export const login = async (req, res) => {};
-
-export const logout = async (req, res) => {};
