d462be1f8a6e965e579cb373d978888e1751a91c
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-08-01 21:33:56 +0200

    trying to connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 // backend/server.js
 import 'dotenv/config.js';
 import app from './app.js';
-import connectDB from './config/db.js';
+import { connectDB } from './config/db.js';
 
 await connectDB();
 
 const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, () =>
-  console.log(`🚀  API ready → http://localhost:${PORT}/api`)
+  console.log(`Backend running on: http://localhost:${PORT}/api`)
 );
