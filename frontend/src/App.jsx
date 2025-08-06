42f9faae35225dc87eb334b4c975f9e552bc6add
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-08-06 20:05:59 +0200

    npm run dev now runs, pre FE update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,6 @@ import Header from './Components/Header.jsx';
 import HomePage from './Pages/HomePage.jsx';
 import Footer from './Components/Footer.jsx';
 import ScrollToTopButton from './Components/ui/ScrollToTopButton.jsx';
-// import AuthPage from './Pages/AuthPage.jsx';
-import SignUp from './Pages/SignUp.jsx';
-import SignIn from './Pages/SignIn.jsx';
-import BlogChoice from './Pages/BlogChoice.jsx';
 
 import './styles.css';
 
@@ -21,10 +17,10 @@ function App() {
             <Route path="/" element={<HomePage />} />
             {/* <Route path="/login" element={<AuthPage mode="signin" />} />
             <Route path="/signup" element={<AuthPage mode="signup" />} /> */}
-            <Route path="/blog-choice" element={<BlogChoice />} />
+            {/* <Route path="/blog-choice" element={<BlogChoice />} /> */}
             {/* Auth routes */}
-            <Route path="/login" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
+            {/* <Route path="/login" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} /> */}
           </Routes>
           <ScrollToTopButton />
         </main>
