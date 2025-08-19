7ebf52f84c16489b614b458d61813c956c968f77
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-08-19 22:27:17 +0200

    hashpwd pre user save or create

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,14 +1,25 @@
 // Read about timestamps in Mongoose: https://mongoosejs.com/docs/timestamps.html
 import mongoose from 'mongoose';
+import { hashPassword, comparePassword } from '../config/bcrypt';
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }, // <- hash later
+    password: { type: String, required: true }, // <- hash later, maybe works now?
     role: { type: String, enum: ['author', 'admin'], default: 'author' },
   },
   { timestamps: true }
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    this.password = await hashPassword(this.password);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default mongoose.model('User', userSchema);
