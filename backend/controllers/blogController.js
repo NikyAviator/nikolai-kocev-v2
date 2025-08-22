15800358adee47b081d47337b429184aa82f4249
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-08-22 19:15:56 +0200

    testing simple get all blogs

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -10,13 +10,26 @@ export const createBlog = async (req, res) => {
       coverImg: req.body.coverImg,
       category: req.body.category,
     });
+
+    res.status(201).json({ status: 'success', data: { blog: newBlog } });
   } catch (error) {
     console.error('Error creating blog:', error);
     res.status(500).send('Internal Server Error');
   }
 };
 
-export const getAllBlogs = (req, res) => res.send('all-blogs');
+export const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res
+      .status(200)
+      .json({ status: 'success', results: blogs.length, data: { blogs } });
+  } catch (error) {
+    console.error('Error fetching blogs:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
 export const getBlogById = (req, res) => res.send(`one-blog ${req.params.id}`);
 export const updateBlog = (req, res) => res.send(`update ${req.params.id}`);
 export const deleteBlog = (req, res) => res.send(`delete ${req.params.id}`);
