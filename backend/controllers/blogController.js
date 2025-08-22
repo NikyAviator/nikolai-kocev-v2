d19b43267ffb28afc1d1d046e2fd71c2e4585809
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-08-22 20:26:06 +0200

    delete blogById

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -39,5 +39,24 @@ export const getBlogById = async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 };
-export const updateBlog = (req, res) => res.send(`update ${req.params.id}`);
-export const deleteBlog = (req, res) => res.send(`delete ${req.params.id}`);
+export const updateBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({ status: 'success', data: { blog } });
+  } catch (error) {
+    console.error('Error updating blog:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+export const deleteBlog = async (req, res) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.status(204).json({ status: 'success', data: null });
+  } catch (error) {
+    console.error('Error deleting blog:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
