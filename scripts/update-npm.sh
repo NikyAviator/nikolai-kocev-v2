d05f34c8aa3bc55e4c6bfdac36c0323cc9d562af
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-08-26 19:04:14 +0200

    npm-update.sh works!

---
+++ scripts/update-npm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Run `npm update` in root, backend, and frontend
+
+# Run shellcheck on it in POSIX mode (-s sh): 
+# shellcheck -s sh bad_script.sh
+
+# Unofficial Bash Strict Mode
+set -euo pipefail
+IFS=$'\n\t'
+
+
+# Fix path
+ROOT="$(cd "$(dirname "$0")/.." && pwd)"
+
+for dir in "$ROOT" "$ROOT/backend" "$ROOT/frontend"; do
+    if [[ -f "$dir/package.json" ]]; then
+        echo "Updating npm packages in $dir"
+       ( cd "$dir" && npm update )
+    else
+        echo "No package.json found in $dir, skipping..."
+    fi
+done
+
+echo "npm packages updated in all three directories." 
