99dd514b602580170b46a95c6be03ef3b0dcaf11
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-09-17 18:55:14 +0200

    chore: remove Node/Express backend (archived via tag/branch)

--- backend/controllers/userController.js
+++
@@ -1,3 +0,0 @@
-export const registerUser = (req, res) => res.send('register-ok');
-export const loginUser = (req, res) => res.send('login-ok');
-export const logoutUser = (req, res) => res.send('logout-ok');
--- backend/routes/v1/public/userRoutesPublic.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/userRoutes.js
-import * as userController from '../../../controllers/userController.js';
-import express from 'express';
-const router = express.Router();
-
-router.post('/login', userController.loginUser);
-router.post('/logout', userController.logoutUser);
-
-export default router;
