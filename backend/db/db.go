c480f6acab6973134399523768fc518787f6e06d
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-09-28 00:35:14 +0200

    working on db.go Connect func

--- backend/db/db.go
+++ backend/db/db.go
@@ -8,23 +8,14 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-var client *mongo.Client
-var ctx context.Context
-var cancel context.CancelFunc
-
-func initDB() {
-	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+func Connect(ctx context.Context, uri, dbName string) (*mongo.Client, *mongo.Database, error) {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
-
-	var err error
-	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
 	if err != nil {
-		panic(err)
+		return err
 	}
+	db := client.Database(dbName)
+	return client, db, nil
 
-	defer func() {
-		if err := client.Disconnect(ctx); err != nil {
-			panic(err)
-		}
-	}()
 }
