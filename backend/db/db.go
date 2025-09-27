3e72afd43048b3916a22dbd69c85d3fff3fabb3d
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-09-27 20:57:54 +0200

    basic db.go

---
+++ backend/db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+var client *mongo.Client
+var ctx context.Context
+var cancel context.CancelFunc
+
+func initDB() {
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var err error
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+}
