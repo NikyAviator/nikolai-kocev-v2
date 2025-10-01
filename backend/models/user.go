7f3ad0aafc9e11bed10616a3e7a96e44bf7fddda
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-10-01 20:22:44 +0200

    Update user.go

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,8 +1,15 @@
 package models
 
 import (
+	"context"
+	"errors"
+	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/nikyaviator/nikolai-kocev-v2/backend/models"
+	"github.com/nikyaviator/nikolai-kocev-v2/backend/utils"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -24,4 +31,53 @@ func (u User) Save() {
 
 func GetAllUsers() []User {
 	return users
+}
+
+func login(c *gin.Context) {
+	var user models.User
+
+	err := c.ShouldBindJSON(&user)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data."})
+		return
+	}
+
+	err = user.ValidateCredentials()
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "invalid credentials"})
+		return
+	}
+
+	token, err := utils.GenerateToken(user.Email, user.ID)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "could not generate token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
+
+}
+
+func (u User) ValidateCredentials() error {
+	// Need to validate credentials
+	// Need to get the password from DB and compare with the provided one
+	// 1) Query from MongoDB
+	var user User
+	err := db.Collection("users").FindOne(context.Background(), bson.M{"email": u.Email}).Decode(&user)
+	if err != nil {
+		return err
+	}
+
+	passwordIsValid := utils.CheckPasswordHash(u.Password, user.Password)
+
+	if !passwordIsValid {
+		return errors.New("invalid password")
+	}
+
+	// If everything is fine, return nil
+
+	return nil
 }
