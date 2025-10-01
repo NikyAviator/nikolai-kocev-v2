d371b4e8da98d67df89239c9c7161b9ddcfa9760
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-10-01 22:21:52 +0200

    Authenticate updated

--- backend/middlewares/auth.go
+++ backend/middlewares/auth.go
@@ -1,8 +1,27 @@
 package middlewares
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/nikyaviator/nikolai-kocev-v2/backend/utils"
 )
 
 func Authenticate(c *gin.Context) {
+	token := c.Request.Header.Get("Authorization")
+
+	if token == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "No token provided"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Not authorized"})
+		return
+	}
+
+	c.Set("userId", userId)
+	c.Next()
 }
