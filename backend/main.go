cee06a650ecd7ae89e4363779dc24f4095e4caf4
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-10-09 17:21:04 +0200

    u main.go, check now db.go

--- backend/main.go
+++ backend/main.go
@@ -4,31 +4,45 @@ import (
 	"context"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nikyaviator/nikolai-kocev-v2/backend/db"
 	"github.com/nikyaviator/nikolai-kocev-v2/backend/routes"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	uri := os.Getenv("MONGODB_URI")
-	port := os.Getenv("PORT")
+	uri := os.Getenv("MONGODB_URI")       // mongodb://localhost:27017/
+	port := os.Getenv("PORT")             // 5000
+	dbName := os.Getenv("MONGODB_DBNAME") // nkv2
+
+	// docs is just a string with a link to the docs
 	docs := "www.mongodb.com/docs/drivers/go/current/"
 	if uri == "" {
 		log.Fatal("Set your 'MONGODB_URI' environment variable. " +
 			"See: " + docs +
 			"usage-examples/#environment-variable")
 	}
-	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+
+	// Create context with timeout - BETTER APPROACH
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	if err != nil {
 		panic(err)
 	}
 	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
+		if err := client.Disconnect(ctx); err != nil {
 			panic(err)
 		}
 	}()
+
+	// Create database reference (store)
+	mongoDB := client.Database(dbName)
+	store := db.NewStore(mongoDB) // Wrap mongoDB in your *db.Store type
 
 	// routes
 	r := gin.Default()
