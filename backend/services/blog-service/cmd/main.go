2897f6d31942d5a695cf0b8bb9b8dea5a0a00872
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-10-15 20:29:47 +0200

    u main.go. env works from root dir

--- backend/services/blog-service/cmd/main.go
+++ backend/services/blog-service/cmd/main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("error loading .env file")
+	}
+
+	mongodbURI := os.Getenv("MONGODB_URI")
+	port := os.Getenv("PORT")
+	jwtSecret := os.Getenv("JWT_SECRET")
+	mongodbName := os.Getenv("MONGODB_NAME")
+
+	log.Println("MongoDB URI:", mongodbURI)
+	log.Println("Port:", port)
+	log.Println("JWT Secret:", jwtSecret)
+	log.Println("MongoDB Name:", mongodbName)
+
+	log.Println("Environment variables loaded successfully")
+	log.Println("Now starting MongoDB instance")
+	log.Println("PPC")
+	// Here you would add the code to start your MongoDB instance or connect to it
+
 }
