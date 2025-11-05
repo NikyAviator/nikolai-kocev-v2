e89bf4a3570b47b7dc0af2ca7c077338e125a2e6
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-11-05 22:49:26 +0100

    impl repo method for CreateBlog

--- backend/services/blog-service/internal/infrastructure/repository/mongo_blog_repo.go
+++ backend/services/blog-service/internal/infrastructure/repository/mongo_blog_repo.go
@@ -1,6 +1,10 @@
 package repository
 
 import (
+	"context"
+
+	"github.com/nikyaviator/nikolai-kocev-v2/backend/services/blog-service/internal/domain"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -10,4 +14,16 @@ type mongoRepository struct {
 
 func NewMongoRepository(db *mongo.Database) *mongoRepository {
 	return &mongoRepository{db: db}
+}
+
+// Implement repository methods here
+func (r *mongoRepository) CreateBlog(ctx context.Context, blog *domain.BlogModel) (*domain.BlogModel, error) {
+	result, err := r.db.Collection("blogs").InsertOne(ctx, blog)
+	if err != nil {
+		return nil, err
+	}
+
+	blog.ID = result.InsertedID.(primitive.ObjectID)
+
+	return blog, nil
 }
