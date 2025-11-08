11f36d7544d943469e76db6ca7610ffd82f181aa
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-11-08 16:48:22 +0100

    added some spacing

--- backend/services/blog-service/internal/infrastructure/repository/mongo_blog_repo.go
+++ backend/services/blog-service/internal/infrastructure/repository/mongo_blog_repo.go
@@ -2,10 +2,13 @@ package repository
 
 import (
 	"context"
+	"time"
 
 	"github.com/nikyaviator/nikolai-kocev-v2/backend/services/blog-service/internal/domain"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type mongoRepository struct {
@@ -16,16 +19,40 @@ func NewMongoRepository(db *mongo.Database) *mongoRepository {
 	return &mongoRepository{coll: db.Collection("blogs")}
 }
 
-// Call
+// Call once to Ensure Indexes
+func (r *mongoRepository) EnsureIndexes(ctx context.Context) error {
+
+	indexModels := []mongo.IndexModel{
+		{ // unique slug
+			Keys:    bson.D{{Key: "slug", Value: 1}},
+			Options: options.Index().SetUnique(true),
+		},
+		{ // sort helper
+			Keys:    bson.D{{Key: "publishedAt", Value: -1}},
+			Options: options.Index().SetName("idx_publishedAt_desc"),
+		},
+	}
+
+	_, err := r.coll.Indexes().CreateMany(ctx, indexModels)
+	return err
+}
 
 // Implement repository methods here
 func (r *mongoRepository) CreateBlog(ctx context.Context, blog *domain.BlogModel) (*domain.BlogModel, error) {
-	result, err := r.db.Collection("blogs").InsertOne(ctx, blog)
+
+	now := time.Now()
+
+	if blog.CreatedAt.IsZero() {
+		blog.CreatedAt = now
+	}
+
+	blog.UpdatedAt = now
+
+	res, err := r.coll.InsertOne(ctx, blog)
 	if err != nil {
 		return nil, err
 	}
-
-	blog.ID = result.InsertedID.(primitive.ObjectID)
+	blog.ID = res.InsertedID.(primitive.ObjectID)
 
 	return blog, nil
 }
