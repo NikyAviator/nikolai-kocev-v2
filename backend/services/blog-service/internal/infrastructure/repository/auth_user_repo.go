8c2ed29a18817a60269ef05a95a8f7898f413726
Nikolai Kocev <nikolai_kocev@hotmail.com>
2025-12-01 18:10:15 +0100

    ensureIndexes users

--- backend/services/blog-service/internal/infrastructure/repository/auth_user_repo.go
+++ backend/services/blog-service/internal/infrastructure/repository/auth_user_repo.go
@@ -4,7 +4,9 @@ import (
 	"context"
 
 	"github.com/nikyaviator/nikolai-kocev-v2/backend/services/blog-service/internal/domain"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type UserRepository interface {
@@ -17,6 +19,14 @@ type MongoUserRepository struct {
 
 func NewMongoUserRepository(db *mongo.Database) *MongoUserRepository {
 	return &MongoUserRepository{coll: db.Collection("users")}
+}
+
+func (r *MongoUserRepository) EnsureIndexes(ctx context.Context) error {
+	models := []mongo.IndexModel{
+		{Keys: bson.D{{Key: "adminEmail", Value: 1}}, Options: options.Index().SetUnique(true)},
+	}
+	_, err := r.coll.Indexes().CreateMany(ctx, models)
+	return err
 }
 
 func (r *MongoUserRepository) Create(ctx context.Context, u domain.User) (domain.User, error) {
